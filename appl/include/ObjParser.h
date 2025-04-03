052abd0911af187c4772d26792edcd20535e2f4b
MarcoCaponera <marco.caponera4@gmail.com>
2025-04-03 18:34:10 +0200

    completed obj parser
    finished objparser and added tests for TryParseObj and Equals operator of the various added structs

--- appl/include/ObjParser.h
+++ appl/include/ObjParser.h
@@ -3,6 +3,37 @@
 #include <fstream>
 #include <vector>
 
+<<<<<<< Updated upstream
+=======
+#define VERTEX_PREFIX "v "
+#define UV_PREFIX "vt "
+#define NORMAL_PREFIX "vn "
+#define FACE_PREFIX "f "
+
+struct Float3
+{
+    float a;
+    float b;
+    float c;
+
+    bool operator==(const Float3& other) const
+    {
+        return a == other.a && b == other.b && c == other.c; 
+    }
+};
+
+struct Float2
+{
+    float a;
+    float b;
+
+    bool operator==(const Float2& other) const
+    {
+        return a == other.a && b == other.b;
+    }
+};
+
+>>>>>>> Stashed changes
 class Tokenizer
 {
 public:
@@ -21,9 +52,39 @@ public:
     }
 };
 
+<<<<<<< Updated upstream
 struct Obj
 {
 
+=======
+struct Vertex
+{
+    Float3 position;
+    Float3 normal;
+    Float2 uv;
+
+    bool operator==(const Vertex& other) const
+    {
+        return position == other.position && normal == other.normal && uv == other.uv;
+    }
+};
+
+struct Triangle
+{
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+
+    bool operator==(const Triangle& other) const 
+    {
+        return v1 == other.v1 && v2 == other.v2 && v3 == other.v3;
+    }
+};
+
+struct Obj
+{
+    std::vector<Triangle> triangles;
+>>>>>>> Stashed changes
 };
 
 class ObjParser
@@ -34,6 +95,7 @@ public:
         std::ifstream file(objPath);
         if(!file.is_open()) return false;
 
+<<<<<<< Updated upstream
         std::string line;
         while(std::getline(file, line))
         {
@@ -44,6 +106,68 @@ public:
         }
 
         return false;
+=======
+        std::vector<Float3> vertexes;
+        std::vector<Float2> uvs;
+        std::vector<Float3> normals;
+
+        std::string line;
+
+        while(std::getline(file, line))
+        {
+            if(line.starts_with(VERTEX_PREFIX))
+            {
+                auto tokens = Tokenizer::Split(line, ' ');
+                Float3 v;
+                v.a = std::stof(tokens[1]);
+                v.b = std::stof(tokens[2]);
+                v.c = std::stof(tokens[3]);
+
+                vertexes.push_back(v);
+            }
+            else if(line.starts_with(UV_PREFIX))
+            {
+                auto tokens = Tokenizer::Split(line, ' ');
+                Float2 uv;
+                uv.a = std::stof(tokens[1]);
+                uv.b = std::stof(tokens[2]);
+
+                uvs.push_back(uv);
+            }
+            else if(line.starts_with(NORMAL_PREFIX))
+            {
+                auto tokens = Tokenizer::Split(line, ' ');
+                Float3 normal;
+                normal.a = std::stof(tokens[1]);
+                normal.b = std::stof(tokens[2]);
+                normal.c = std::stof(tokens[3]);
+
+                normals.push_back(normal);
+            }
+            else if(line.starts_with(FACE_PREFIX))
+            {
+                Triangle t;
+                // get pointer to first triangle struct field
+                Vertex* currVertex = &t.v1;
+                auto spacedTokens = Tokenizer::Split(line, ' ');
+                for(size_t i = 1; i < spacedTokens.size(); ++i)
+                {
+                    auto tabbedTokens = Tokenizer::Split(spacedTokens[i], '/');
+                    size_t vertexIndex = std::stoi(tabbedTokens[0]);
+                    size_t uvIndex = std::stoi(tabbedTokens[1]);
+                    size_t normalIndex = std::stoi(tabbedTokens[2]);
+
+                    Vertex v{vertexes[vertexIndex - 1], normals[normalIndex - 1], uvs[uvIndex - 1]};
+                    // assign currVertex and increment ptr
+                    *currVertex++ = v;
+                }
+
+                outObj.triangles.push_back(t);
+            }
+        }
+
+        return true;
+>>>>>>> Stashed changes
     }
 
 private:
--- test/src/ObjParserTest.cpp
+++ test/src/ObjParserTest.cpp
@@ -13,6 +13,194 @@ CLOVE_TEST(TokenizerWithFourTokens) {
    CLOVE_STRING_EQ(tokens[1].c_str(), "-1.000000");
    CLOVE_STRING_EQ(tokens[2].c_str(), "1.000000");
    CLOVE_STRING_EQ(tokens[3].c_str(), "0.000000");
+<<<<<<< Updated upstream
 
    CLOVE_PASS();
+=======
+}
+
+CLOVE_TEST(Float3EqualsWithDifferentFloat3)
+{
+   Float3 f1{0.0f, 1.0f, 3.0f};
+   Float3 f2{1.0f, 0.0f, 5.0f};
+
+   CLOVE_IS_FALSE(f1 == f2);
+}
+
+CLOVE_TEST(Float3EqualsWithEqualFloat3)
+{
+   Float3 f1{0.0f, 1.0f, 3.0f};
+   Float3 f2{0.0f, 1.0f, 3.0f};
+
+   CLOVE_IS_TRUE(f1 == f2);
+}
+
+CLOVE_TEST(VertexEqualsWithDifferentVertexes)
+{
+   Float3 vn1{0.f, 0.f, 1.f};
+   Float3 vpos1{1.0f, -1.0f, 0.0f};
+   Float2 vuv1{1.0f, 0.0f};
+   Vertex v1{vpos1, vn1, vuv1};
+
+   Float3 vn2{0.f, 1.f, 5.f};
+   Float3 vpos2{-4.0f, -1.0f, 0.0f};
+   Float2 vuv2{1.0f, 3.0f};
+   Vertex v2{vpos2, vn2, vuv2};
+
+   CLOVE_IS_FALSE(v1 == v2);
+}
+
+CLOVE_TEST(VertexEqualsWithEqualVertexes)
+{
+   Float3 vn1{0.f, 0.f, 1.f};
+   Float3 vpos1{1.0f, -1.0f, 0.0f};
+   Float2 vuv1{1.0f, 0.0f};
+   Vertex v1{vpos1, vn1, vuv1};
+
+   Float3 vn2{0.f, 0.f, 1.f};
+   Float3 vpos2{1.0f, -1.0f, 0.0f};
+   Float2 vuv2{1.0f, 0.0f};
+   Vertex v2{vpos2, vn2, vuv2};
+
+   CLOVE_IS_TRUE(v1 == v2);
+}
+
+CLOVE_TEST(TriangleEqualsWithDifferentTriangles)
+{
+   Float3 vn{1.0f, 0.0f, 0.0f};
+
+   // triangle 1
+   // vertex 1
+   Float3 pos_t1_v1{1.0f, -1.0f, 0.0f};
+   Float2 uv_t1_v1{1.0f, 0.0f};
+   Vertex t1_v1{pos_t1_v1, vn, uv_t1_v1};
+   // vertex 2
+   Float3 pos_t1_v2{-1.0f, -1.0f, 0.0f};
+   Float2 uv_t1_v2{0.0f, 0.0f};
+   Vertex t1_v2{pos_t1_v2, vn, uv_t1_v2};
+   // vertex 3
+   Float3 pos_t1_v3{-1.0f, 1.0f, 0.0f};
+   Float2 uv_t1_v3{0.0f, 1.0f};
+   Vertex t1_v3{pos_t1_v3, vn, uv_t1_v3};
+
+   // triangle 2
+   // vertex 1
+   Float3 pos_t2_v1{1.0f, 1.0f, 0.0f};
+   Float2 uv_t2_v1{1.0f, 1.0f};
+   Vertex t2_v1{pos_t2_v1, vn, uv_t2_v1};
+   // vertex 2
+   Float3 pos_t2_v2{1.0f, -1.0f, 0.0f};
+   Float2 uv_t2_v2{1.0f, 0.0f};
+   Vertex t2_v2{pos_t2_v2, vn, uv_t2_v2};
+   // vertex 3
+   Float3 pos_t2_v3{-1.0f, 1.0f, 0.0f};
+   Float2 uv_t2_v3{0.0f, 1.0f};
+   Vertex t2_v3{pos_t2_v3, vn, uv_t2_v3};
+
+   Triangle t1{t1_v1, t1_v2, t1_v3};
+   Triangle t2{t2_v1, t2_v2, t2_v3};
+
+   CLOVE_IS_FALSE(t1 == t2);
+}
+
+CLOVE_TEST(TriangleEqualsWithEqualTriangles)
+{
+   Float3 vn{1.0f, 0.0f, 0.0f};
+
+   // triangle 1
+   // vertex 1
+   Float3 pos_t1_v1{1.0f, -1.0f, 0.0f};
+   Float2 uv_t1_v1{1.0f, 0.0f};
+   Vertex t1_v1{pos_t1_v1, vn, uv_t1_v1};
+   // vertex 2
+   Float3 pos_t1_v2{-1.0f, -1.0f, 0.0f};
+   Float2 uv_t1_v2{0.0f, 0.0f};
+   Vertex t1_v2{pos_t1_v2, vn, uv_t1_v2};
+   // vertex 3
+   Float3 pos_t1_v3{-1.0f, 1.0f, 0.0f};
+   Float2 uv_t1_v3{0.0f, 1.0f};
+   Vertex t1_v3{pos_t1_v3, vn, uv_t1_v3};
+
+   // triangle 2
+   // vertex 1
+   Float3 pos_t2_v1{1.0f, -1.0f, 0.0f};
+   Float2 uv_t2_v1{1.0f, 0.0f};
+   Vertex t2_v1{pos_t2_v1, vn, uv_t2_v1};
+   // vertex 2
+   Float3 pos_t2_v2{-1.0f, -1.0f, 0.0f};
+   Float2 uv_t2_v2{0.0f, 0.0f};
+   Vertex t2_v2{pos_t2_v2, vn, uv_t2_v2};
+   // vertex 3
+   Float3 pos_t2_v3{-1.0f, 1.0f, 0.0f};
+   Float2 uv_t2_v3{0.0f, 1.0f};
+   Vertex t2_v3{pos_t2_v3, vn, uv_t2_v3};
+
+   Triangle t1{t1_v1, t1_v2, t1_v3};
+   Triangle t2{t2_v1, t2_v2, t2_v3};
+
+   CLOVE_IS_TRUE(t1 == t2);
+}
+
+CLOVE_TEST(ObjParserWithInvalidPath)
+{
+   Obj obj;
+   ObjParser objParser;
+
+   CLOVE_IS_FALSE(objParser.TryParseObj("", obj));
+}
+
+CLOVE_TEST(ObjParserWithCorrectPath)
+{
+   std::string objPath{CLOVE_EXEC_BASE_PATH()};
+   objPath += "/resources/quad.obj";
+
+   Obj obj;
+   ObjParser objParser;
+   
+   // read successful
+   CLOVE_IS_TRUE(objParser.TryParseObj(objPath, obj)); 
+
+   // triangles vector not empty
+   CLOVE_INT_NE(obj.triangles.size(), 0);
+
+   // expected size 2
+   CLOVE_INT_EQ(obj.triangles.size(), 2);
+
+   // same for every vertex
+   Float3 vn{0.f, 0.f, 1.f};
+
+   // triangle 1
+   // vertex 1
+   Float3 pos_t1_v1{1.0f, -1.0f, 0.0f};
+   Float2 uv_t1_v1{1.0f, 0.0f};
+   Vertex t1_v1{pos_t1_v1, vn, uv_t1_v1};
+   // vertex 2
+   Float3 pos_t1_v2{-1.0f, -1.0f, 0.0f};
+   Float2 uv_t1_v2{0.0f, 0.0f};
+   Vertex t1_v2{pos_t1_v2, vn, uv_t1_v2};
+   // vertex 3
+   Float3 pos_t1_v3{-1.0f, 1.0f, 0.0f};
+   Float2 uv_t1_v3{0.0f, 1.0f};
+   Vertex t1_v3{pos_t1_v3, vn, uv_t1_v3};
+
+   // triangle 2
+   // vertex 1
+   Float3 pos_t2_v1{1.0f, 1.0f, 0.0f};
+   Float2 uv_t2_v1{1.0f, 1.0f};
+   Vertex t2_v1{pos_t2_v1, vn, uv_t2_v1};
+   // vertex 2
+   Float3 pos_t2_v2{1.0f, -1.0f, 0.0f};
+   Float2 uv_t2_v2{1.0f, 0.0f};
+   Vertex t2_v2{pos_t2_v2, vn, uv_t2_v2};
+   // vertex 3
+   Float3 pos_t2_v3{-1.0f, 1.0f, 0.0f};
+   Float2 uv_t2_v3{0.0f, 1.0f};
+   Vertex t2_v3{pos_t2_v3, vn, uv_t2_v3};
+
+   Triangle t1{t1_v1, t1_v2, t1_v3};
+   Triangle t2{t2_v1, t2_v2, t2_v3};
+
+   CLOVE_IS_TRUE(t1 == obj.triangles[0]);
+   CLOVE_IS_TRUE(t2 == obj.triangles[1]);
+>>>>>>> Stashed changes
 }
